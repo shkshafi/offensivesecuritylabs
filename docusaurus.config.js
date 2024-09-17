64b020d93f57028b41073fe3d378be8232b92bee
Shafi Ur Rahman <shaik.shafi.ur.rahman@gmail.com>
2024-09-17 23:58:18 +0530

    after build5

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://offensivesecuritylabs.com/kb/build',
+  url: 'https://offensivesecuritylabs.com/kb/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/kb/build',
