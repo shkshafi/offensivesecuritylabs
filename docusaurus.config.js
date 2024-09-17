0ac9ac0048da31104d799815da06ba0542c96e73
Shafi Ur Rahman <shaik.shafi.ur.rahman@gmail.com>
2024-09-18 00:27:44 +0530

    after build10

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://offensivesecuritylabs.com/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/kb/build/',
+  baseUrl: '/kb/',
 
    
   // GitHub pages deployment config.
