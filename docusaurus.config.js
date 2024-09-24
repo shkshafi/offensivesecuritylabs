ee06d4adb5e46a3ab8d0b903d2c13454a24add36
Shafi Ur Rahman <shafiurrahman@Shafis-MacBook-Pro.local>
2024-09-24 22:21:23 +0530

    test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/kb/',
 
-   
+  // swizzle: './swizzle',
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'facebook', // Usually your GitHub org/user name.
---
+++ src/theme/TOC/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TOC from '@theme-original/TOC';
+
+export default function TOCWrapper(props) {
+  return (
+    <>
+      <TOC {...props} />
+      <br/>
+                <a href="https://www.netlify.com" target="_blank" rel="noreferrer noopener" aria-label="Deploys by Netlify">
+                  <img src="https://www.netlify.com/img/global/badges/netlify-color-accent.svg" alt="Deploys by Netlify" width="114" height="51" />
+                </a>
+    </>
+  );
+}
