0b1b953c82fb4974e03936d9eda4834b930539f1
Shafi Ur Rahman <shaik.shafi.ur.rahman@gmail.com>
2024-09-29 17:58:16 +0530

    test

--- kb/build/assets/js/36994c47.583472d8.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkoff_sec_labs=self.webpackChunkoff_sec_labs||[]).push([[9858],{5516:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
--- kb/build/assets/js/5e95c892.9ddab62a.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkoff_sec_labs=self.webpackChunkoff_sec_labs||[]).push([[9647],{7121:(e,s,r)=>{r.r(s),r.d(s,{default:()=>t});r(6540);var a=r(4164),c=r(1213),u=r(7559),f=r(2831),l=r(781),n=r(4848);function t(e){return(0,n.jsx)(c.e3,{className:(0,a.A)(u.G.wrapper.docsPages),children:(0,n.jsx)(l.A,{children:(0,f.v)(e.route.routes)})})}}}]);
