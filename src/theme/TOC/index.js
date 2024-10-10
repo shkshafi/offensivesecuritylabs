310bd40b4a0fca891a0d822b36b85dd471474b41
Shafi Ur Rahman <shaik.shafi.ur.rahman@gmail.com>
2024-10-10 18:23:12 +0530

    final

--- src/theme/TOC/index.js
+++ src/theme/TOC/index.js
@@ -10,6 +10,15 @@ export default function TOCWrapper(props) {
       <a aria-label="Deploys by ">
                   <img src="https://www.netlify.com/img/global/badges/netlify-color-accent.svg" alt="Deploys by " width="114" height="51" />
                 </a>
+                <script async custom-element="amp-ad" src="https://cdn.ampproject.org/v0/amp-ad-0.1.js"></script>
+                <amp-ad width="100vw" height="320"
+     type="adsense"
+     data-ad-client="ca-pub-1260667564079492"
+     data-ad-slot="1240870063"
+     data-auto-format="rspv"
+     data-full-width="">
+  <div overflow=""></div>
+</amp-ad>
     </>
     
   );
